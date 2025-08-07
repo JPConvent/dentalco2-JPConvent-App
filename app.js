f30965f00c60b6bcd5611c9ea3f54d5596a74b03
JPConvent <mail@jpconvent.de>
2025-08-07 23:10:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Platzhalter für spätere Scope-Berechnung und PDF-Export
 document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault();
-  alert("Berechnung erfolgt im nächsten Schritt – PDF-Export wird vorbereitet.");
+  alert("Berechnung erfolgt im nächsten Schritt – PDF Export wird vorbereitet.");
 });
